3894a4f2c3cea07e8ae44debc934eb45790cdcb0
Jonathan Raines <jr0278@bristol.ac.uk>
2024-05-28 09:52:21 +0100

    Fix svg format

--- fetch_and_make_map.py
+++ fetch_and_make_map.py
@@ -4,49 +4,62 @@ import requests
 
 BASE_URL = "https://maps2.bristol.gov.uk/server1/rest/services/base/1880s_CSEpoch1_128dpi_mk3/MapServer/tile/5/"
 X_START = 18297
-X_END = 18304
+X_END = 18299  # 18304
 Y_START = 14093
-Y_END = 14100
+Y_END = 14095  # 14100
 SIZE = 256
 
 
 def main(base_url, x_start, x_end, y_start, y_end):
-    image_urls = []
-    for x, y in itertools.product(range(x_start, x_end), range(y_start, y_end)):
-        image_url = f"{base_url}{y}/{x}"
-        image_urls.append(image_url)
-    svg = generate_grid(image_urls, x_end - x_start, y_end - y_start, SIZE, SIZE)
+    image_urls = make_urls(base_url, x_start, x_end, y_start, y_end)
+    svg = make_grid(image_urls, x_end - x_start, y_end - y_start, SIZE, SIZE)
     with open("output.svg", "w", encoding="utf8") as f:
         f.write(svg)
 
 
-def fetch_image(url):
-    response = requests.get(url, timeout=5)
-    return base64.b64encode(response.content).decode("utf-8")
+def make_urls(base_url, x_start, x_end, y_start, y_end):
+    image_urls = []
+    for x, y in itertools.product(range(x_start, x_end), range(y_start, y_end)):
+        image_url = f"{base_url}{y}/{x}"
+        image_urls.append(image_url)
+    return image_urls
 
 
-def generate_svg(image_url, width, height):
-    encoded_image = fetch_image(image_url)
-    svg = f"""
-    <svg width="{width}" height="{height}">
-      <image href="data:image/jpg;base64,{encoded_image}" x="0" y="0" width="{width}" height="{height}" />
-    </svg>\
-    """
-    return svg
-
-
-def generate_grid(image_urls, grid_width, grid_height, cell_width, cell_height):
-    svgs = []
+def make_grid(image_urls, grid_width, grid_height, cell_width, cell_height):
+    image_components = []
     for i, url in enumerate(image_urls):
         x = (i % grid_width) * cell_width
         y = (i // grid_width) * cell_height
-        svg = generate_svg(url, cell_width, cell_height)
-        svgs.append(f'<g transform="translate({x}, {y})">{svg}</g>')
+        encoded_image = fetch_image(url)
+        svg = make_image_component(encoded_image, x, y, cell_width, cell_height)
+        image_components.append(f'<g transform="translate({x}, {y})">{svg}</g>')
+    width = grid_width * cell_width
+    height = grid_height * cell_height
     return (
-        f'<svg width="{grid_width * cell_width}" height="{grid_height * cell_height}">'
-        + "".join(svgs)
-        + "</svg>"
+        f'<svg width="{width}"\n height="{height}"\n'
+        + f'viewBox="0 0 {width} {height}"\n'
+        + 'version="1.1" id="bristol-map"\n'
+        + 'xml:space="preserve"\n'
+        + 'xmlns:xlink="http://www.w3.org/1999/xlink"\n'
+        + 'xmlns="http://www.w3.org/2000/svg"\n'
+        + 'xmlns:svg="http://www.w3.org/2000/svg"\n>\n'
+        + '<g id="layer1">\n'
+        + "".join(image_components)
+        + "\n</g>\n</svg>"
     )
+
+
+def fetch_image(url):
+    response = requests.get(url, timeout=5)
+    with open("output.jpg", "wb") as f:
+        f.write(response.content)
+    return base64.b64encode(response.content).decode("utf-8")
+
+
+def make_image_component(encoded_image, x, y, width, height):
+    return f"""
+      <image\n\twidth="{width}"\n\theight="{height}"\n\txlink:href="data:image/jpeg;base64,{encoded_image}"\n\tx="{x}"\n\ty="{y}"\n/>
+    """
 
 
 if __name__ == "__main__":
